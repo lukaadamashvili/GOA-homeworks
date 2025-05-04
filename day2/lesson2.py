d1456a15aaed176be384b7f25e3c9e5e10eb3449
lukaadamashvili <lukaadamashvili01@gmail.com>
2025-05-04 18:58:49 +0400

    third commit

---
+++ day2/lesson2.py
@@ -0,0 +1,19 @@
+#ცვლადი არის იმისთვის,დაიმახსოვროს სიტყვა,შეინახოს და ნებისმიერ დრო გამოვიყენოთ.
+#გამოიყენება,რომ ცვლადის სახელი დაწoრის და ტერმინალში გამოიტანოს ცვლადის მნიშვნელობა
+num1 = 15
+num2 = 10
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+
+
+name = "luka"
+surname = "adamashvli"
+age = 12
+height = 1.58
+
+print(surname * age)
+print("hello i am", name)
+print("i am" surname)
---
+++ homework2.py
@@ -0,0 +1,21 @@
+num1=10
+num2=5
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+
+#  + amatebs - aklebs * amravlebs / gayops
+
+name="luka"
+surname="adamashvili"
+
+print(name * 11)
+
+
+
+print(name + " " + surname)
+
+print("luka adamashvili" * 100)
+
