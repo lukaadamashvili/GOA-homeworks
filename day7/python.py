8959d101dbce9371486529d603bf4c657eb0bfd6
lukaadamashvili <lukaadamashvili01@gmail.com>
2025-06-13 19:20:56 +0400

    (second comit)

---
+++ day7/python.py
@@ -0,0 +1,22 @@
+num1 = 5
+num2 = 7
+sum = num1 + num2
+print(sum)
+
+
+price = 50
+price = 80
+print(price)
+
+
+name = input("enter your name here: ")
+age = input("enter your age here: ")
+print(name)
+print(age)
+
+
+#DEBBUGING არის შეცდომის ჩასწორება
+
+NAME = "LUKA"
+name = "luka"
+print (NAME)
