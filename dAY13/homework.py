de4b6ba7a8d4266c5998afe919dd51730d0fae4b
lukaadamashvili <lukaadamashvili01@gmail.com>
2025-07-25 15:19:03 +0400

    commit

---
+++ dAY13/homework.py
@@ -0,0 +1,16 @@
+for i in range(0, 100, 2):
+    print(i)
+
+for i in range(1, 100, 2):
+    print(i)
+
+name = "luka"
+for i in range(100):
+    print(name)
+
+num1 = 75802048760303
+for i in range(num1):
+    print (6 >= num1)
+
+for i in range(0, 200, 10):
+    print(i)
---
+++ dAY13/python.py
@@ -0,0 +1,30 @@
+for i in range (10):
+    print(i)
+
+for i in range (100):
+    print("luka")
+
+name = "luka"
+for i in name:
+    print(i)
+
+for i in range (0, 20, 2):
+    print(i)
+
+#for loop იწერება როგორც for i in და გამოიყენება იმისთვის რომ, გავაკეთოტ ჩამონათვალი იგივე algorithm
+
+for i in range (0, 20, 2):
+    print(i)
+
+for i in range (75, 150, 2):
+    print(i)
+
+start=5
+stop=10
+
+for i in range(start, stop):
+    print(i)
+
+num1 = 76559875466
+for i in range(num1):
+    print(4 >= num1)
